26323c263ec8e19e96317d696c257401c93b9413
awil897 <93953412+awil897@users.noreply.github.com>
2022-11-22 17:02:46 -0600

    Add files via upload

---
+++ EnterpriseWorkflowTools/Get-EWFReportUrl.ps1
@@ -0,0 +1,19 @@
+﻿Function Get-EWFReportUrl {
+    param
+    (
+    [String]
+    [Parameter(Mandatory)]
+    $WFGUID,
+    
+    [String]
+    [Parameter(Mandatory)]
+    $WFURL,
+    
+    [Parameter(Mandatory)]
+    [ValidateSet("CategoryPermissions","QueuePermissions","InstitutionPermissions")]
+    $Type
+    )
+
+    $outLink = "https:\\$wfurl\Reports\$type.$wfguid.html"
+    return $outlink
+}
---
+++ EnterpriseWorkflowTools/Invoke-TestReportsFolder.ps1
@@ -0,0 +1,7 @@
+﻿Function Invoke-TestReportsFolder {
+    $FolderName = "C:\inetpub\wwwroot\Reports"
+    if (!(Test-Path $FolderName)){
+        #PowerShell Create directory if not exists
+        New-Item $FolderName -ItemType Directory | out-null
+    }
+}
