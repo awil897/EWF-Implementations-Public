60a7b212ae8b88496841620deaf6eaeae88aff10
awil897 <93953412+awil897@users.noreply.github.com>
2022-11-22 17:02:22 -0600

    Delete Modules/EnterpriseWorkflowTools directory

--- Modules/EnterpriseWorkflowTools/Invoke-TestReportsFolder.ps1
+++
@@ -1,7 +0,0 @@
-﻿Function Invoke-TestReportsFolder {
-    $FolderName = "C:\inetpub\wwwroot\Reports"
-    if (!(Test-Path $FolderName)){
-        #PowerShell Create directory if not exists
-        New-Item $FolderName -ItemType Directory | out-null
-    }
-}
